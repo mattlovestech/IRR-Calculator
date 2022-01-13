608f9b3e9c217afef58ca80599905b7e308f7f9c
Matthew Manning <matthewmanning@apple.com>
2022-01-12 23:37:21 -0500

    started to frame out app page and determine componets needed for calculation

--- src/App.js
+++ src/App.js
@@ -1,24 +1,44 @@
 import logo from './logo.svg';
 import './App.css';
 import Card from '@mui/material/Card';
+import CardContent from "@mui/material/CardContent";
+import Typography from "@mui/material/Typography";
+import CardActions from "@mui/material/CardActions";
+import Button from "@mui/material/Button";
+
+
 function App() {
   return (
-    <Card className="App" width={500}>
+
+    <div className="App">
+
+
+
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+          <div style={{padding: "40px"}}>
+      <Typography variant={"h3"} color="text.white" gutterBottom>
+        <b>I</b>nternal <b>R</b>ate of <b>R</b>eturn <small>Calculator</small>
+      </Typography>
+          <hr/>
+
+
+              <Typography variant={"h4"}  color="text.white">
+                  Wait....what is Internal Rate of Return?
+              </Typography>
+              <br/>
+              <Button variant="outlined" > <Typography variant={"h6"}  color="text.white">
+                  Learn more
+              </Typography></Button>
+              <br/>
+
+              <Typography  sx={{ fontSize: 15 }} color="text.grey">
+                  The IRR helps managers determine which potential projects add value and are worth undertaking. The advantage of expressing project values as a rate is the clear hurdle it provides. As long as the financing cost is less than the rate of potential return, the project adds value.
+              </Typography>
+
+          </div>
+
       </header>
-    </Card>
+    </div>
   );
 }
 
