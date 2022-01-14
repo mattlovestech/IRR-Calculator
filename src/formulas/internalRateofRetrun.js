c5164a84639fdaef19a3aa7f3968cc7e12986fa4
Matthew Manning <matthewmanning@apple.com>
2022-01-13 23:26:59 -0500

    created both formulas for NPV and IRR

---
+++ src/formulas/internalRateofRetrun.js
@@ -0,0 +1,34 @@
+import Button from "@mui/material/Button";
+import netPresentValue from "./netPresentValue";
+import Typography from "@mui/material/Typography";
+const { irr } = require('node-irr')
+
+export default function internalRateOfReturn(data) {
+    const rawData = {0: -500000, 1: 210000, 2: 237000, 3: 265000}
+    //const data = [-10, -10, 21]
+    console.log(irr(data))
+
+    // //  let initialInvestment = 500000
+    // // let initial = (-(initialInvestment)/((1+discountRate)^0))
+    // let sumOfYears = 0
+    // // let myArray = {0: -500000, 1: 210000, 2: 237000, 3: 265000}
+    //
+    //
+    // let npv = 1
+    // let npvTwo = 0
+    // let discount = 1
+    // let discountRateOne = 1.5
+    // let discountRateTwo = 1.25
+    // let discountRateThree = 1.06
+    // while (npv !== 0) {
+    //     npv = netPresentValue(discount,{0: -500000, 1: 210000, 2: 237000, 3: 265000})
+    //     if (npv > 0) {
+    //        discount = discount - 0.01
+    //     } else {
+    //         discount = discount + 0.01
+    //     }
+    //     console.log("NPV:", npv, " discount", discount)
+    //
+    // }
+    return data
+}
--- src/formulas/netPresentValue.js
+++ src/formulas/netPresentValue.js
@@ -1,18 +1,17 @@
-export default function NetPresentValue() {
-    let npv = 1
-    let discountRate = 1.06
-    let initialInvestment = 500000
-    let initial = (-(initialInvestment)/((1+discountRate)^0))
+export default function NetPresentValue(discountRate,cashflowByYearObj) {
+   
+   
+  //  let initialInvestment = 500000
+   // let initial = (-(initialInvestment)/((1+discountRate)^0))
     let sumOfYears = 0
-    npv = initial + sumOfYears
-    let myArray = {0: -500000, 1: 210000, 2: 237000, 3: 265000}
-    let year1 = 237000/1.1236
+   // let myArray = {0: -500000, 1: 210000, 2: 237000, 3: 265000}
+   
 
-    let i = 0, len = Object.keys(myArray).length;
+    let i = 0, len = Object.keys(cashflowByYearObj).length;
     while (i < len) {
         // your code
         let currentDiscountRate = (Math.pow(discountRate, i))
-        let currentCashFlow = myArray[i]/currentDiscountRate
+        let currentCashFlow = cashflowByYearObj[i]/currentDiscountRate
         sumOfYears = sumOfYears + currentCashFlow
         console.log(currentCashFlow)
       //  console.log(myArray[i+1])
@@ -20,6 +19,6 @@ export default function NetPresentValue() {
         i++ //increases the value of i after the code runs
 
     }
-    console.log(sumOfYears)
-    return initial
+    console.log("Net Present Value: " + sumOfYears)
+    return sumOfYears
 }
