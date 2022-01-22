c1ea7a103829b2b6cfb5a8b84b69c62e265f46f0
Matthew Manning <matthewmanning@apple.com>
2022-01-22 09:26:21 -0500

    turned off autocomplete

--- src/components/IrrInput.js
+++ src/components/IrrInput.js
@@ -4,7 +4,7 @@ import {Input} from "@mui/material";
 import React from "react";
 import TextField from "@mui/material/TextField";
 export default function IrrInput(props) {
-   return(<><TextField type="number" sx={{ input: { color: "white" } }} id={props.id} value={props.value} onChange={props.onChange} label={props.label}  variant="filled" /><br/><br/></>
+   return(<><TextField autoComplete={"off"} type="number" sx={{ input: { color: "white" } }} id={props.id} value={props.value} onChange={props.onChange} label={props.label}  variant="filled" /><br/><br/></>
    // <Input style={{fontSize: "20px", paddingTop: "10px", fontFamily: "oswald", color: "white !important"}}  type={"number"} id={props.id} value={props.value} onChange={props.onChange}  />
    )
 }
