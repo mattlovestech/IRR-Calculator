91ac87428187103fe1b13e8358c08e64dd8dfcca
Matthew Manning <matthewmanning@apple.com>
2022-01-22 18:37:48 -0500

    updated the youtube video used in the video

--- src/tabs/whatIsIrr.js
+++ src/tabs/whatIsIrr.js
@@ -17,8 +17,10 @@ export default function WhatIsIRR() {
 
             <h4>TL:DR - just watch this video I made👇🏿</h4>
             <div style={{textAlign: "center"}}>
-                <iframe width={"250px"} height={"250px"} src="https://www.youtube.com/embed/aSnPIfBjlHc" />
-            </div>
+                <iframe width="560" height="315" src="https://www.youtube.com/embed/3P4syXdF7mg"
+    title="YouTube video player" frameBorder="0"
+    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+    allowFullScreen/>            </div>
             </div>
         </>
 
