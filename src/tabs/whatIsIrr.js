b0c113986dd98429630e608b3f230d2a3a1f8ebe
Matthew Manning <matthewmanning@apple.com>
2022-01-23 19:14:35 -0500

    edited what is IRR page

--- src/tabs/whatIsIrr.js
+++ src/tabs/whatIsIrr.js
@@ -17,17 +17,18 @@ export default function WhatIsIRR() {
 
             <h4>TL:DR - just watch this video I made👇🏿</h4>
             <div style={{textAlign: "center"}}>
-                <iframe width="560" height="315" src="https://www.youtube.com/embed/3P4syXdF7mg"
+                <iframe width="100%" height="315" src="https://www.youtube.com/embed/3P4syXdF7mg"
     title="YouTube video player" frameBorder="0"
     allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
     allowFullScreen/>            </div>
                 <h1>IRR explained 👀 </h1>
-                <p>The internal rate of return (IRR) is a metric used in financial analysis to estimate the profitability of potential investments.</p>
+                <p>Internal rate of return (IRR) is a key metric used in financial analysis to estimate the profitability of potential investments, this metric is used often (not exclusively) in Real Estate when comparing investments.</p>
                 <strong>IRR Can Help when Comparing</strong>
-                <p>Generally speaking, the higher an internal rate of return, the more desirable an investment is to undertake. IRR is uniform for investments of varying types and, as such, can be used to rank multiple prospective investments or projects on a relatively even basis. In general, when comparing investment options with other similar characteristics, the investment with the highest IRR probably would be considered the best.</p>
-
-                <div style={{backgroundColor: "black"}}>
+                <p>Generally, the greater an internal rate of return, the more fruitful the investment/venture is.
+                   The power of IRR is that when comparing investment options with other similar characteristics, the investment with the highest IRR probably would be considered the best.</p>
+                <div style={{backgroundColor: "black", color: "white", padding: "10px"}}>
                     <h2> IRR Formula</h2>
+                    <img height="288" width={"100%"} src="IRRFormula.png" alt={"IRR Formula"} />
                 </div>
 
             </div>
