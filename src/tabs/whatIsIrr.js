4188b9a17ba49ec03541f6a3f165a4b9ff0d4acd
Matthew Manning <45296246+mattlovestech@users.noreply.github.com>
2023-10-08 21:12:50 +0000

    fixed image height

--- src/tabs/whatIsIrr.js
+++ src/tabs/whatIsIrr.js
@@ -27,7 +27,7 @@ export default function WhatIsIRR() {
                    The power of IRR is that when comparing investment options with other similar characteristics, the investment with the highest IRR probably would be considered the best.</p>
                 <div style={{backgroundColor: "black", color: "white", padding: "10px"}}>
                     <h2> IRR Formula</h2>
-                    <img height="288" width={"100%"} src={IRRFormulaImage} alt={"IRR Formula"} />
+                    <img height="auto" width={"100%"} src={IRRFormulaImage} alt={"IRR Formula"} />
                 </div>
 
             </div>
