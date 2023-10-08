be603f69115b3199cc6fff76fa9f339e49a4ab27
Matthew Manning <45296246+mattlovestech@users.noreply.github.com>
2023-10-08 21:19:10 +0000

    updated definionts

--- src/tabs/whatIsIrr.js
+++ src/tabs/whatIsIrr.js
@@ -11,8 +11,9 @@ export default function WhatIsIRR() {
                 color: "black",
                 opacity: ".8"
             }}>
-                <p><strong>Learn about Internal Rate of Return</strong> <br/> Internal Rate of Return, it is an important data point
-                when making investment decisions</p>
+                <h1>About Internal Rate of Return</h1> 
+                <br/> The discount rate at which the net present value (NPV) of a series of projected cash flows equals zero, serving as a key metric to evaluate and compare the potential profitability of investments.
+</p>
 
             <h4>TL:DR - just watch this video I made👇🏿</h4>
             <div style={{textAlign: "center"}}>
@@ -21,7 +22,14 @@ export default function WhatIsIRR() {
     allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
     allowFullScreen/>            </div>
                 <h1>IRR explained 👀 </h1>
-                <p>Internal rate of return (IRR) is a key metric used in financial analysis to estimate the profitability of potential investments, this metric is used often (not exclusively) in Real Estate when comparing investments.</p>
+                <p>
+    <strong>Internal Rate of Return (IRR)</strong> represents the annualized effective compounded return rate at which an investment equates its net present value (NPV) to zero. In essence, it quantifies the estimated rate of growth a project is expected to generate. While the metric is most commonly associated with financial markets, it's ubiquitously utilized across a plethora of sectors, from corporate finance to private equity, and notably, in Real Estate for discerning the potential profitability of property investments.
+
+    When an IRR exceeds a company's required rate of return, the investment is typically considered favorable, as it's poised to yield returns that surpass the cost of capital. Conversely, when the IRR is below the cost of capital, it might indicate potential losses. This binary perspective underscores the IRR's profound utility in comparing various investment opportunities: A higher IRR usually alludes to a potentially higher return on investment (ROI), rendering it a superior choice relative to other options. 
+
+    IRR does not account for factors like the size of the investment or the project's absolute profitability. As a result,  investors often use it in comnbination with other financial metrics, ensuring a comprehensive view of an investment's potential.
+</p>
+
                 <strong>IRR Can Help when Comparing</strong>
                 <p>Generally, the greater an internal rate of return, the more fruitful the investment/venture is.
                    The power of IRR is that when comparing investment options with other similar characteristics, the investment with the highest IRR probably would be considered the best.</p>
